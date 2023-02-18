c0dee6b715c775eb757926fdacf04175e1e04523
FandiNurrokhim <109939812+FandiNurrokhim@users.noreply.github.com>
2023-02-18 18:48:42 +0700

    Add files via upload

---
+++ Bg change/index.js
@@ -0,0 +1,9 @@
+function ubahBg(){
+    document.body.style.backgroundColor = "#0093E9";
+}
+function ubahBg2(){
+    document.body.style.backgroundColor = "#16A085";
+}
+function ubahBg3(){
+    document.body.style.backgroundColor = "#A00101 ";
+}
